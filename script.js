c3f78a612ab25900423c1202949f68baef1bd7ef
DecoMacie <119458392+DecoMacie@users.noreply.github.com>
2023-07-19 22:55:16 +0100

    Updated JS code

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const digitsBtn = document.querySelectorAll(".digits");
 const operationBtn = document.querySelectorAll(".operation");
-let topScreen = document.querySelector("operand-one");
-let bottomScreen = document.querySelector("operand-two");
+let topScreen = document.querySelector(".operand-one");
+let bottomScreen = document.querySelector(".operand-two");
 let delBtn = document.querySelector(".delete");
 let allclearBtn = document.querySelector(".allClear");
 let iqualsBtn = document.querySelector(".iquals");
@@ -10,13 +10,43 @@ console.log(digitsBtn);
 console.log(operationBtn);
 console.log(delBtn);
 console.log(allclearBtn);
+console.log(topScreen);
 console.log(iqualsBtn);
 
 class Calculator {
   constructor(topScreen, bottomScreen) {
     this.topScreen = topScreen;
     this.bottomScreen = bottomScreen;
+    this.allClear();
   }
+
+  allClear() {
+    this.op1 = "";
+    this.op2 = "";
+    this.operation = undefined;
+  }
+
+  nrConcat(digit) {
+    this.op1 = digit;
+  }
+
+  showOnScreen() {
+    this.bottomScreen.innerText = this.op1;
+  }
+
+  iqual() {}
+
+  chooseOp() {}
+
+  deleteItem() {}
 }
 
-const calculator = new Calculator();
+const calculator = new Calculator(topScreen, bottomScreen);
+
+digitsBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    calculator.nrConcat(btn.innerText);
+    calculator.showOnScreen();
+    console.log("click");
+  });
+});
